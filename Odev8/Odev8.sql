bd3306001d2e0a9a5bc58ab0cbd94e9951833429
Oguzhan Elkapmis <oguzhan.elkapmis@gmail.com>
2022-04-02 23:25:19 +0300

    Update Odev8.sql

--- Odev8/Odev8.sql
+++ Odev8/Odev8.sql
@@ -4,5 +4,13 @@ SET name = 'Oguzhan',
 	email = 'deneme@gmail.com'
 WHERE id = 2;
 
+SET name = 'Daniel', 
+	birthday = '1970-11-14',
+	email = 'daniel_narrow@gmail.com'
+WHERE id = 15;
+
 DELETE FROM employee
 WHERE name = 'Jo';
+
+DELETE FROM employee
+WHERE id = 38;
